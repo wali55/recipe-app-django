66da58ab24ee67252f5f38ce3bc129d3703e68cf
wali55 <walisantunu@gmail.com>
2024-03-30 22:34:26 +0600

    accounts

--- accounts/models.py
+++ accounts/models.py
@@ -14,20 +14,20 @@ class MyUserManager(BaseUserManager):
         )
 
         user.set_password(password)
-        user.save(self._db)
+        user.save(using=self._db)
         return user
     
     def create_superuser(self, first_name, last_name, email, phone, password=None):
-        user = self.model(
+        user = self.create_user(
             first_name = first_name,
             last_name = last_name,
-            email = email,
+            email = self.normalize_email(email),
             phone = phone,
             password = password,
         )
 
         user.is_admin = True
-        user.save(self._db)
+        user.save(using=self._db)
         return user
     
 
@@ -42,17 +42,19 @@ class MyUser(AbstractBaseUser):
     phone = models.CharField(max_length=100)
 
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
 
     def __str__(self):
         return self.email
     
     def has_perm(self, perm, obj=None):
-        return True
+        return self.is_admin
     
     def has_module_perms(self, app_label):
         return True
